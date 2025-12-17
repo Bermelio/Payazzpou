04e057743ff60b8782597bf6dd91054059b1a603
Bermelio <facundoperalta258@gmail.com>
2025-12-16 23:17:41 -0300

    errorcito con ./background.js

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-const { response } = require("express");
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "fetchPlayers") {
     fetch(`https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=${request.appid}`)
-      .then(response => response.json())
-      .then(data => sendResponse({ success: true, data }))
-      .catch(error => sendResponse({ success: false, error: error.message }));
-      return true;
+      .then(res => res.json())
+      .then(data => {
+        sendResponse({ success: true, data });
+      })
+      .catch(error => {
+        sendResponse({ success: false, error: error.message });
+      });
+    return true;
   }
 });
